ca7089f9b36109e3ebc78ffc99a3542fff62bc06
itlowly <lowly0201@hotmail.com>
2017-03-14 16:03:56 +0800

    添加fragment分页

---
+++ version 2/app/src/main/java/com/itlowly/twenty/fragment/HelpFragment.java
@@ -0,0 +1,20 @@
+package com.itlowly.twenty.fragment;
+
+import com.itlowly.twenty.R;
+
+/**
+ * Copyright 2016 Lelight
+ * All right reserved.
+ * <p>
+ * Created by itLowly
+ * <p>
+ * on 2017/2/6 18:16
+ */
+
+public class HelpFragment extends BaseFragment {
+
+    @Override
+    public void initViews() {
+        inflateRootView(R.layout.fragment_help);
+    }
+}
---
+++ version 2/app/src/main/java/com/itlowly/twenty/fragment/home/TagAdapter.java
@@ -0,0 +1,29 @@
+package com.itlowly.twenty.fragment.home;
+
+import android.content.Context;
+
+import com.itlowly.twenty.baseadapter.CommonAdapter;
+import com.itlowly.twenty.baseadapter.ViewHolder;
+
+import java.util.List;
+
+/**
+ * Copyright 2016 Lelight
+ * All right reserved.
+ * <p>
+ * Created by itLowly
+ * <p>
+ * on 2017/2/6 18:02
+ */
+
+public class TagAdapter extends CommonAdapter<String> {
+
+    public TagAdapter(Context context, List<String> datas, int layoutId) {
+        super(context, datas, layoutId);
+    }
+
+    @Override
+    public void convert(ViewHolder holder, String str) {
+
+    }
+}
